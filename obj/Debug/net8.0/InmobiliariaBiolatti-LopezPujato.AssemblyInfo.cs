b3fc49cc32135e1946d74f77cf9e688310bc4068
pablojlpk <113646557+pablojlpk@users.noreply.github.com>
2024-03-23 16:48:32 -0300

    -

--- obj/Debug/net8.0/InmobiliariaBiolatti-LopezPujato.AssemblyInfo.cs
+++ obj/Debug/net8.0/InmobiliariaBiolatti-LopezPujato.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InmobiliariaBiolatti-LopezPujato")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58a5ca74146c11d686bd00a8383099dcfd31009c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1e93397220d629b206628fb37972f635d852f3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("InmobiliariaBiolatti-LopezPujato")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InmobiliariaBiolatti-LopezPujato")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
