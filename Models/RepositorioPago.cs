e6bf8f18222c1e27aa83f0959467d8076a3f8449
Ana luz Biolatti <anluz_1420@hotmail.com>
2024-04-02 16:23:21 -0300

    repo

--- Models/RepositorioPago.cs
+++ Models/RepositorioPago.cs
@@ -4,6 +4,7 @@ namespace InmobiliariaBiolatti_LopezPujato.Models;
 public class RepositorioPago
 {
     readonly string ConnectionString = "Server=localhost;Database=inmodotnet;User=root;Password=;";
+    
     public RepositorioPago()
     {
 
@@ -39,16 +40,59 @@ public class RepositorioPago
         }
         return pagos;
     }
+    public int Baja(int idpago)
+    {
+        int res = -1;
+        using (var connection = new MySqlConnection(ConnectionString))
+        {
+            var sql = @$"DELETE FROM pagos WHERE {nameof(Pago.idpago)} = @id";
+            using (var command = new MySqlCommand(sql, connection))
+            {
+                command.Parameters.AddWithValue($"@{nameof(Pago.idpago)}", idpago);
+
+                connection.Open();
+                res = command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+        return res;
+
+    }
+    public int altaPago (Pago p)
+    {
+        int res = -1;
+        using (var connection = new MySqlConnection(ConnectionString))
+        {
+            var sql = $"INSERT INTO pago (idcontrato, Monto, Afecha) VALUES ({p.idcontrato},{p.Monto},'{p.Afecha}')";
+            using (var command = new MySqlCommand(sql, connection))
+            {
+                connection.Open();
+                res = command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+        return res;
+    }
 
 
+    public int ModificaPago (Pago p)
+    {
+        int res = -1;
+        using (var connection = new MySqlConnection(ConnectionString))
+        {
+            var sql = $"UPDATE pago SET idcontrato = {nameof(p.idcontrato)}, Monto = {nameof(p.Monto)}, Afecha = '{nameof(p.Afecha)}' WHERE {nameof(Pago.idpago)} = {nameof(p.idpago)}";
+            using (var command = new MySqlCommand(sql, connection))
+            {
+                command.Parameters.AddWithValue($"@{nameof(Pago.idpago)}", p.idpago);
+                command.Parameters.AddWithValue($"@{nameof(Pago.idcontrato)}", p.idcontrato);
+                command.Parameters.AddWithValue($"@{nameof(Pago.Monto)}", p.Monto);
+                command.Parameters.AddWithValue($"@{nameof(Pago.Afecha)}", p.Afecha);
+                connection.Open();
+                res = command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+        return res;     
+    }
 
-
-
-    
-    
-    
-    
-    
-    
-    
     }
--- obj/Debug/net8.0/InmobiliariaBiolatti-LopezPujato.AssemblyInfo.cs
+++ obj/Debug/net8.0/InmobiliariaBiolatti-LopezPujato.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InmobiliariaBiolatti-LopezPujato")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a53534168f893d6978d49074928a0d004134e5af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b7361c399ad73a31abd1b2241b3ab907619a5bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("InmobiliariaBiolatti-LopezPujato")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InmobiliariaBiolatti-LopezPujato")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
