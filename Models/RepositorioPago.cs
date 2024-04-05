39e38383361b7a044c63550f8448dc8707be9e2b
Ana luz Biolatti <anluz_1420@hotmail.com>
2024-04-05 01:03:23 -0300

    ,

--- Models/RepositorioPago.cs
+++ Models/RepositorioPago.cs
@@ -1,98 +1,136 @@
 using MySql.Data.MySqlClient;
 
-namespace InmobiliariaBiolatti_LopezPujato.Models;
-public class RepositorioPago
+namespace InmobiliariaBiolatti_LopezPujato.Models
 {
-    readonly string ConnectionString = "Server=localhost;Database=inmodotnet;User=root;Password=;";
-    
-    public RepositorioPago()
+    public class RepositorioPago
     {
+        readonly string ConnectionString = "Server=localhost;Database=inmodotnet;User=root;Password=;";
 
-    }
-    
-    public IList<Pago> GetPagos()
-    {
-        var pagos = new List<Pago>();       
-
-
-        using (var connection = new MySqlConnection(ConnectionString))
+        public RepositorioPago()
         {
-            var sql = $"SELECT {nameof(Pago.idpago)}, {nameof(Pago.idcontrato)}, {nameof(Pago.importe)}, {nameof(Pago.fpago)} FROM pagos where borrado=0 ORDER by fpago ASC";
 
-            using (var command = new MySqlCommand(sql, connection))
+        }
+
+        public IList<Pago> GetPagos()
+        {
+            var pagos = new List<Pago>();
+
+            using (var connection = new MySqlConnection(ConnectionString))
             {
-                connection.Open();
-                using (var reader = command.ExecuteReader())
+                var sql = $"SELECT {nameof(Pago.idpago)}, {nameof(Pago.idcontrato)}, {nameof(Pago.importe)}, {nameof(Pago.fpago)} FROM pago WHERE borrado = 0 ORDER BY fpago ASC";
+
+                using (var command = new MySqlCommand(sql, connection))
                 {
-                    while (reader.Read())
-                    {   
-                        pagos.Add(new Pago
-                        {   
-                            idpago = reader.GetInt32(nameof(Pago.idpago)),
-                            idcontrato = reader.GetInt32(nameof(Pago.idcontrato)),
-                            importe = reader.GetDecimal(nameof(Pago.importe)),
-                            fpago = reader.GetDateTime(nameof(Pago.fpago))
-                        });
+                    connection.Open();
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            pagos.Add(new Pago
+                            {
+                                idpago = reader.GetInt32(nameof(Pago.idpago)),
+                                idcontrato = reader.GetInt32(nameof(Pago.idcontrato)),
+                                importe = reader.GetDecimal(nameof(Pago.importe)),
+                                fpago = reader.GetDateTime(nameof(Pago.fpago))
+                            });
+                        }
                     }
                     connection.Close();
                 }
             }
+            return pagos;
         }
-        return pagos;
-    }
-    public int Baja(int idpago)
-    {
-        int res = -1;
-        using (var connection = new MySqlConnection(ConnectionString))
+
+          public Pago GetPago(int id)
         {
-            var sql = @$"DELETE FROM pagos WHERE {nameof(Pago.idpago)} = @id";
-            using (var command = new MySqlCommand(sql, connection))
+            Pago? pago = null;
+
+            using (var connection = new MySqlConnection(ConnectionString))
             {
-                command.Parameters.AddWithValue($"@{nameof(Pago.idpago)}", idpago);
+                var sql = $"SELECT {nameof(Pago.idpago)}, {nameof(Pago.idcontrato)}, {nameof(Pago.importe)}, {nameof(Pago.fpago)} FROM pago WHERE idpago = @idpago";
 
-                connection.Open();
-                res = command.ExecuteNonQuery();
-                connection.Close();
+                using (var command = new MySqlCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@idpago", id);
+                    connection.Open();
+                    using (var reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            pago = new Pago
+                            {
+                                idpago = reader.GetInt32(nameof(Pago.idpago)),
+                                idcontrato = reader.GetInt32(nameof(Pago.idcontrato)),
+                                importe = reader.GetDecimal(nameof(Pago.importe)),
+                                fpago = reader.GetDateTime(nameof(Pago.fpago))
+                            };
+                        }
+                    }
+                    connection.Close();
+                }
             }
+            return pago;
         }
-        return res;
 
-    }
-    public int altaPago (Pago p)
-    {
-        int res = -1;
-        using (var connection = new MySqlConnection(ConnectionString))
+        public int Baja(int idpago)
         {
-            var sql = $"INSERT INTO pago (idcontrato, Monto, Afecha) VALUES ({p.idcontrato},{p.importe},'{p.fpago}')";
-            using (var command = new MySqlCommand(sql, connection))
+            int res = -1;
+            using (var connection = new MySqlConnection(ConnectionString))
             {
-                connection.Open();
-                res = command.ExecuteNonQuery();
-                connection.Close();
+                var sql = $"DELETE FROM pago WHERE {nameof(Pago.idpago)} = @idpago";
+                using (var command = new MySqlCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@idpago", idpago);
+
+                    connection.Open();
+                    res = command.ExecuteNonQuery();
+                    connection.Close();
+                }
             }
+            return res;
+
         }
-        return res;
-    }
 
-
-    public int ModificaPago (Pago p)
-    {
-        int res = -1;
-        using (var connection = new MySqlConnection(ConnectionString))
+        public int altaPago(Pago p)
         {
-            var sql = $"UPDATE pago SET idcontrato = {nameof(p.idcontrato)}, Monto = {nameof(p.importe)}, Afecha = '{nameof(p.fpago)}' WHERE {nameof(Pago.idpago)} = {nameof(p.idpago)}";
-            using (var command = new MySqlCommand(sql, connection))
+            int res = -1;
+            using (var connection = new MySqlConnection(ConnectionString))
             {
-                command.Parameters.AddWithValue($"@{nameof(Pago.idpago)}", p.idpago);
-                command.Parameters.AddWithValue($"@{nameof(Pago.idcontrato)}", p.idcontrato);
-                command.Parameters.AddWithValue($"@{nameof(Pago.importe)}", p.importe);
-                command.Parameters.AddWithValue($"@{nameof(Pago.fpago)}", p.fpago);
-                connection.Open();
-                res = command.ExecuteNonQuery();
-                connection.Close();
-            }
-        }
-        return res;     
-    }
+                var sql = $"INSERT INTO pago (idcontrato, Monto, Afecha) VALUES (@idcontrato, @importe, @fpago)";
+                using (var command = new MySqlCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@idcontrato", p.idcontrato);
+                    command.Parameters.AddWithValue("@importe", p.importe);
+                    command.Parameters.AddWithValue("@fpago", p.fpago);
 
+                    connection.Open();
+                    res = command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+            return res;
+        }
+
+        public int ModificaPago(Pago p)
+        {
+            int res = -1;
+            using (var connection = new MySqlConnection(ConnectionString))
+            {
+                var sql = $"UPDATE pago SET idcontrato = @idcontrato, Monto = @importe, Afecha = @fpago WHERE {nameof(Pago.idpago)} = @idpago";
+                using (var command = new MySqlCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@idpago", p.idpago);
+                    command.Parameters.AddWithValue("@idcontrato", p.idcontrato);
+                    command.Parameters.AddWithValue("@importe", p.importe);
+                    command.Parameters.AddWithValue("@fpago", p.fpago);
+
+                    connection.Open();
+                    res = command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+            return res;
+        }
     }
+}
+
