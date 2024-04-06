cc91df4062a324534d83b391ef513cc8e7c26db3
pablojlpk <113646557+pablojlpk@users.noreply.github.com>
2024-04-06 14:53:52 -0300

    -reparar

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace InmobiliariaBiolatti_LopezPujato.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
