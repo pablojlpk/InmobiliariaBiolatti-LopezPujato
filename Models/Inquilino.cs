22fe0024895e499e32cfb4029bc87d9c1195baa6
Ana luz Biolatti <anluz_1420@hotmail.com>
2024-04-19 21:19:16 -0300

    FORMULARIOS VALIDADOS

--- Models/Inquilino.cs
+++ Models/Inquilino.cs
@@ -1,12 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace InmobiliariaBiolatti_LopezPujato.Models;
 
     public  class Inquilino
     {
         public int? idinquilino { get; set; }
+        [Required(ErrorMessage = "El nombre es requerido")]
         public string? nombre { get; set; }
+        [Required(ErrorMessage = "El apellido es requerido")]
         public string? apellido { get; set; }
+        [Required(ErrorMessage = "El dni es requerido")]
         public int? dni { get; set; }
+        [Required(ErrorMessage = "El mail es requerido")]
         public string? mail { get; set; }
+        [Required(ErrorMessage = "La clave es requerida")]
         public string? clave { get; set; }
         public bool? borrado { get; set; }
         public override string ToString(){
